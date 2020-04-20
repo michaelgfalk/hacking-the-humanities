f8dc4e416d52904a753318794a9bfa00b508992b
Falkie <35712723+michaelgfalk@users.noreply.github.com>
2020-04-20 16:00:58 +1000

    welcome notebook

---
+++ assignment-2/pandavas.py
@@ -0,0 +1,8 @@
+pandavas = [
+    {'name':'Yudhishthira','gender':'M', 'species':'demigod', 'family':'Pandavas', 'profession':'spearman and charioteer','spouse':'Draupadi'},
+    {'name':'Bhima','gender':'M', 'species':'demigod', 'family':'Pandavas', 'profession':'mace wielder','spouse':'Draupadi'},
+    {'name':'Arjuna','gender':'M', 'species':'demigod', 'family':'Pandavas', 'profession':'archer','spouse':'Draupadi'},
+    {'name':'Nakula','gender':'M', 'species':'demigod', 'family':'Pandavas', 'profession':'swordsman','spouse':'Draupadi'},
+    {'name':'Sahadeva','gender':'M', 'species':'demigod', 'family':'Pandavas', 'profession':'swordsman','spouse':'Draupadi'},
+    {'name':'Karna','gender':'M', 'species':'demigod', 'family':'Pandavas', 'profession':'King of Anga','spouse':None}
+]
